c8ef724cb2aad4ff2e8030847851aed0aa2ea06e
nahkampf <kreuzweg@gmail.com>
2024-07-12 00:22:01 +0200

    Remove status bars for biomonitor

--- _foot.php
+++ _foot.php
@@ -1,4 +1,10 @@
+<?php
+if($config["TERMINAL_TYPE"] != "monitor") {
+?>
     <div class="bottombar">* Haven network: ONLINE. </div>
+<?php
+}
+?>
 </div>
 </body>
 </html>
--- _topbar.php
+++ _topbar.php
@@ -1,4 +1,10 @@
+<?php
+if($config["TERMINAL_TYPE"] != "monitor") {
+?>
     <div class="topbar">
         <div class="topbar-left"><?=($config["MODE"]=="dev") ? "[DEV] " : "";?><?=$topbar_title?></div>    
         <div class="topbar-right"><?=date("H:i:s")?></div>    
     </div>
+<?php
+}
+?>
